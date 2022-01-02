4b688a97fd89bcf0b5acb69db1277ced08863d26
MirekRogowski <93225975+MirekRogowski@users.noreply.github.com>
2022-01-02 15:11:49 +0100

    Update weather.py
    read_api

--- weather.py
+++ weather.py
@@ -14,3 +14,17 @@ dict_weather = {"Rain": "Będzie padać.",
                 "Clouds": "Nie będzie padać"
                 }
 
+
+def read_api():
+    url = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"
+    querystring = {"q": "Konin", "lat": "35", "lon": "139", "cnt": "10", "units": "metric or imperial"}
+    headers = {
+        'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+        'x-rapidapi-key': sys.argv[1]
+    }
+    if not os.path.isfile("out.jason"):
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        out_url = response.json()
+        with open("out.json", 'w') as json_file:
+             json.dump(out_url, json_file)
+    write_loop(read_json_file()) if not os.path.isfile("data.csv") else write_loop_add(read_json_file())
